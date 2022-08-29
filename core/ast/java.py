0d22c7499e19ac030e3a52ffa5f5125e998d29f9
Akiba <akegarasu.akiba@gmail.com>
2022-08-30 01:16:36 +0800

    conflict

--- core/ast/java.py
+++ core/ast/java.py
@@ -1,6 +1,21 @@
 import javalang
+from abc import ABC
 
-if __name__ == "__main__":
-    tree = javalang.parse.parse("package javalang.brewtab.com; class Test {}")
-    print(tree)
+from .model import AstImpl
+
+
+class JavaAst(AstImpl, ABC):
+
+    def __init__(self, code: str):
+        super().__init__()
+        self.code = code
+
+    def parse(self):
+        """
+        impl java ast parser method here
+        :return:
+        """
+        tree = javalang.parse.parse("package javalang.brewtab.com; class Test {}")
+        print(tree)
+
 
